d8de5dfb86b62607ada5a51d3a02c0e4454a0a9f
Asim Bilal <asimbilal@Asims-MacBook.local>
2016-04-16 20:38:51 +0500

    add signup view and add login feature

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -16,54 +16,28 @@
         <!-- Main content -->
         <section class="content">
 
+        <?php
+            if( isset($data) ) {
+                echo($data['message']);
+            } 
+        ?>
+
             <!-- Main row -->
             <div class="row">
                 <div class="col-md-12">
 
-         
-
                     <div class="box box-success">
                         <div class="box-header with-border">
-                            <h3 class="box-title">Add new Category</h3>
+                            <h3 class="box-title">Add new Custom Fields</h3>
                         </div><!-- /.box-header -->
                         <!-- form start -->
-                        <form id="defaultForm" class="form-horizontal" action="<?php echo site_url('Admin/setEvent') ?>" method="post" >
+                        <form id="defaultForm" class="form-horizontal" action="<?php echo site_url('Admin/addNewCustomField') ?>" method="post" >
                             <div class="box-body">
 
                                 <div class="form-group">
                                     <label for="" class="col-sm-2 control-label">Title</label>
                                     <div class="col-sm-6">
-                                        <input type="text" name="event_title" class="form-control" id="" placeholder="">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="" class="col-sm-2 control-label">Description</label>
-                                    <div class="col-sm-6">
-                                        <textarea class="form-control" rows="3" name="event_description"></textarea>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="" class="col-sm-2 control-label">Category</label>
-                                    <div class="col-sm-6">
-                                       <select class="form-control" name="category_id">
-                                            <option value="">Select</option>
-                                           <?php foreach($category as $cat):?>
-                                           <option value="<?php echo $cat['cat_id']?>"><?php echo $cat['cat_name']?></option>
-                                           <?php endforeach;?>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="" class="col-sm-2 control-label">Start Date</label>
-                                    <div class="col-sm-6">
-                                        <input type="text" name="event_startDate" class="form-control datepicker"  id="" placeholder="">
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="" class="col-sm-2 control-label">End Date</label>
-                                    <div class="col-sm-6">
-                                        <input type="text" name="event_endDate" class="form-control datepicker" id="" placeholder="">
+                                        <input type="text" name="custom_field" class="form-control" id="" placeholder="" required>
                                     </div>
                                 </div>
 
