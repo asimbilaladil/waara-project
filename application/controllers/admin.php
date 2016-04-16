56d32e10ae4337cf87302181d4acbbe730fc9659
moiz <mmoiz129>
2016-04-16 18:09:39 +0500

    fix

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,27 +1,46 @@
 <?php
 class Admin extends CI_Controller {
 
-	function index()
-	{
-		$this->load->view('admin/common/header');
-		$this->load->view('admin/common/sidebar');
-		$this->load->view('admin/index');
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('AdminModel');
+    }
+
+    function index()
+    {
+        $this->load->view('admin/common/header');
+        $this->load->view('admin/common/sidebar');
+        $this->load->view('admin/index');
         $this->load->view('admin/common/footer');
 
-	}
+    }
 
 
-	function login()
-	{
-		$this->load->view('admin/login');
-	}
+    function login() {
+        $this->load->view('admin/login');
+    }
 
-
-	function admin_login_check() {
-		$admin_email = $this->input->post('email', true);
+    //when admin login button is click
+    function admin_login_check() {
+        $admin_email = $this->input->post('email', true);
         $admin_password = $this->input->post('password', true);
- 		$this->load->view('admin/login/',$admin_email);
-	}
+        $result = $this->AdminModel->admin_login_check_info($admin_email, $admin_password);
+
+        //if query found any result i.e userfound
+        if($result) {
+            $data['user_id'] = $result->user_id;
+            $data['message'] = 'Your are successfully Login && your session has been start';
+            $this->session->set_userdata($data);
+            redirect('admin/');
+
+        }else{
+            $data['message'] = ' Your Email ID or Password is invalid  !!!!! ';
+            $this->session->set_userdata($data);
+            redirect('admin/login');
+        }
+
+
+    }
 
 }
 ?>
