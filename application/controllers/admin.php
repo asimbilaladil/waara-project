33634a64e3ddd8b5b2f031f1e35bcf8602cb66f2
moiz <mmoiz129>
2016-04-16 16:18:34 +0500

    fix

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -3,18 +3,25 @@ class Admin extends CI_Controller {
 
 	function index()
 	{
-
-		$this->load->view('/admin/index');
-
-	}
-
-
-	function home()
-	{
 		$this->load->view('admin/common/header');
 		$this->load->view('admin/common/sidebar');
-		$this->load->view('admin/admin_home');
+		$this->load->view('admin/index');
         $this->load->view('admin/common/footer');
+
 	}
+
+
+	function login()
+	{
+		$this->load->view('admin/login');
+	}
+
+
+	function admin_login_check() {
+		$admin_email = $this->input->post('email', true);
+        $admin_password = $this->input->post('password', true);
+ 		$this->load->view('admin/login/',$admin_email);
+	}
+
 }
 ?>
