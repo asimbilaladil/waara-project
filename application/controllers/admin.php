0cfbf5939cbf5203cb91f6593eb7b1db2908eaf9
Asim Bilal <asimbilal@Asims-MacBook.local>
2016-04-27 01:16:16 +0500

    add login and logout

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -108,29 +108,6 @@ class Admin extends CI_Controller {
 
     }
 
-
-
-    //when admin login button is click
-    function admin_login_check() {
-        echo $admin_email = $this->input->post('email', true);
-        echo $admin_password = md5($this->input->post('password', true));
-        $result = $this->AdminModel->admin_login_check_info($admin_email, $admin_password);
-
-        //if query found any result i.e userfound
-        if($result) {
-            $data['user_id'] = $result->user_id;
-            $data['message'] = 'Your are successfully Login && your session has been start';
-            $this->session->set_userdata($data);
-            redirect('admin/');
-
-        }else{
-            $data['message'] = ' Your Email ID or Password is invalid  !!!!! ';
-            $this->session->set_userdata($data);
-           // redirect('admin/login');
-        }
-
-    }
-
     /**
      * Insert Custom field data
      * 1. @param : customField
