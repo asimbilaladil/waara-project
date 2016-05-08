aa930ef806fe81ed30b486c51eac21b4ee89fd5f
Asim Bilal <asimbilal@Asims-MacBook.local>
2016-05-08 22:57:48 +0500

    view shift

--- application/views/website/waara.php
+++ application/views/website/waara.php
@@ -145,6 +145,30 @@
                                 </h5>
                             </div>
                         </li>
+                        <li>
+                            <div class="cs-text">
+                                <h5>
+                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                        <div class="cs-event-detail-holder">
+                                            <div class="row">
+                                                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                                                    <div class="cs-event-detail-heading">
+                                                        <h6 class="cs-color"><i class="cs-color icon-uniF103"></i> Shift:</h6>
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+                                                    <div class="cs-event-detail-date-time">
+                                                        <div class="cs-post-title">
+                                                            <h3><?php  echo  ($data->shift == 1? "Evening": ($data->shift == 2? "Morning" :"both"))  ;?></h3>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </h5>
+                            </div>
+                        </li>                        
                     </ul>
                 </div>
             </div>
