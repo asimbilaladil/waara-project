01e0074d95775413a6ce6cfa23641572ddf323d3
Muhammad Moiz <muhammadmoiz129@gmail.com>
2018-02-03 03:21:24 -0600

    renaming files

---
+++ application/controllers/Login.php
@@ -0,0 +1,54 @@
+<?php
+class Login extends CI_Controller {
+
+    public function __construct(){
+        parent::__construct();
+
+            $this->load->model('AdminModel');
+        error_reporting(0);
+
+    }
+
+
+
+    function index() {
+        
+        if($this->input->post()) {
+           
+            $data = array (
+                'email' => $this->input->post('email', true),
+                'password' => md5($this->input->post('password', true) )
+            );
+            $result = $this->AdminModel->admin_login_check_info($data);
+
+            //if query found any result i.e userfound
+            if($result) {
+
+                $data['type'] = $result->type;
+                if( $data['type'] != 'User' ){
+
+                    $data['user_id'] = $result->user_id;
+                    $data['message'] = 'Your are successfully Login && your session has been start';
+                    $data['jk_id'] = $result->jk_id;
+                    $this->session->set_userdata($data);
+                    redirect('admin/');
+
+                } else{ 
+                    $data['message'] = ' Your Email ID or Password is invalid  !!!!! ';
+                    redirect('login/');                    
+                }
+            }else{
+                $data['message'] = ' Your Email ID or Password is invalid  !!!!! ';
+                redirect('login/');
+            }
+
+
+        } else {
+            $this->load->view('admin/login');
+        }
+
+    }
+
+
+}
+?>
