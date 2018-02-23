8c2dc2a8fa892f7d203be80cbb5a9cf77ef3d514
Muhammad Moiz <muhammad.moiz@venturedive.com>
2018-02-23 20:58:57 +0500

    fix

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://waara.ca/';
-$config['base_url'] = 'http://localhost:8888/waara/';
+$config['base_url'] = 'http://waara.ca/';
+//$config['base_url'] = 'http://localhost:8888/waara/';
 
 /*
 |--------------------------------------------------------------------------
